bf9a08e9a6db83ccf5daf42d31c6d30903b4113f
Shashwat Pratap Singh <shashwatpratapsinghh@gmail.com>
2026-06-22 23:13:36 +0530

    Add solution for Maximum Sum of an Hourglass (java)

---
+++ LeetCode/2428-maximum-sum-of-an-hourglass/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int maxSum = Integer.MIN_VALUE;
+        
+        for (int i = 0; i <= m - 3; i++) {
+            for (int j = 0; j <= n - 3; j++) {
+                int currentSum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2] +
+                                 grid[i + 1][j + 1] +
+                                 grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2];
+                maxSum = Math.max(maxSum, currentSum);
+            }
+        }
+        
+        return maxSum;
+    }
+}
